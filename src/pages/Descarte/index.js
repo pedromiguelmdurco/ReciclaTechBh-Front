d759571f01e91e7fe28f7bbc74e9e142b51cbc9e
Lucas Silva <lukasgss37@gmail.com>
2024-06-12 19:57:11 -0300

    carrossel inicial

--- src/pages/Descarte/index.js
+++ src/pages/Descarte/index.js
@@ -1,44 +1,33 @@
-import { useState, useEffect } from "react";
-import { Col, Row, Container, CarouselItem, CarouselCaption } from "react-bootstrap";
-import { Carousel } from "react-bootstrap";
+import { useState } from "react";
+import Carrosel from "../../components/Carrossel/Carrosel";
 
-
-import logo from '../../assets/teste-removebg-preview.png'
 import { Link } from "react-router-dom";
 
 function Descarte() {
+  const [linha, setLinha] = useState([]);
 
-    const [linha, setLinha] = useState([]);
-
-    return (
-
-        <div className="flex-col">
-            <div className="flex w-full">
-                {/* Divisão de imagens carrosel */}
-                <div className="bg-blue-900 h-80 w-1/2"></div>
-                <div className="bg-black h-80 w-1/2" ></div>
-            </div>
-            <div className="flex-col">
-                {/* Divisão Quero descartar */}
-                <h1 className="text-[#5C832F] mx-2 my-2" >Quero descartar</h1>
-                {/* Cards */}
-                <div className="flex justify-between mx-2">
-                    <div className="bg-slate-400 h-72 w-40 hover:opacity-50">
-                    <Link to={'/mapa'} >aaaa</Link>
-                    </div>
-                    <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
-                    <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
-                    <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
-                    <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
-                    <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
-                </div>
-
-
-            </div>
-
+  return (
+    <div className="flex-col">
+      <div className="flex w-full">
+        <Carrosel />
+      </div>
+      <div className="flex-col">
+        {/* Divisão Quero descartar */}
+        <h1 className="text-[#5C832F] mx-2 my-2">Quero descartar</h1>
+        {/* Cards */}
+        <div className="flex justify-between mx-2">
+          <div className="bg-slate-400 h-72 w-40 hover:opacity-50">
+            <Link to={"/mapa"}>aaaa</Link>
+          </div>
+          <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
+          <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
+          <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
+          <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
+          <div className="bg-slate-400 h-72 w-40 hover:opacity-50"></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
 
 export default Descarte;
