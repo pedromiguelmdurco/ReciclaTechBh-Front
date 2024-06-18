ac787fca51859fb394570a9efe025945f2c30f45
Lucas Silva <lukasgss37@gmail.com>
2024-06-18 18:59:18 -0300

    Correção login e filtro por linha marrom

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,81 +1,78 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import './header.css';
-import logo from '../../assets/logo.png'
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import api from "../../services/api";
+import "./header.css";
 
+function Header() {
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
 
-function Header(){
+  function Entrar() {
+    async function loadLogins() {
+      try {
+        const response = await api.post("users/login", {
+          email: login,
+          password: senha,
+        });
 
-    const[login,setLogin] = useState('');
-    const[senha,setSenha] = useState('');
-    const navigate = useNavigate();
+        api.defaults.headers.common.Authorization = `Bearer ${response.data.accessToken}`;
+        localStorage.setItem("Token", response.data.accessToken);
+        navigate("/adimin");
 
-    
-      function Entrar() {
-        async function loadLogins(){
-            try {
-                const response = await api.post("users/login", {
-                    email: login,
-                    password: senha
-                });
-                
-                api.defaults.headers.common.Authorization = `Bearer ${response.data.accessToken}`;
-                localStorage.setItem("Token", response.data.accessToken);
-                navigate('/adimin');
-
-                setLogin('');
-                setSenha('');
-            }
-            
-            catch(error){
-                if (error.response) {
-                    // O servidor respondeu com um status fora da faixa 2xx
-                    console.log('Erro no response:', error.response.data);
-                    alert("Usuário ou senha estão incorretos");
-                } else if (error.request) {
-                    // A requisição foi feita mas não houve resposta
-                    console.log('Erro no request:', error.request);
-                } /*else {
+        setLogin("");
+        setSenha("");
+      } catch (error) {
+        if (error.response) {
+          // O servidor respondeu com um status fora da faixa 2xx
+          console.log("Erro no response:", error.response.data);
+          alert("Usuário ou senha estão incorretos");
+        } else if (error.request) {
+          // A requisição foi feita mas não houve resposta
+          console.log("Erro no request:", error.request);
+        } /*else {
                     // Alguma outra coisa aconteceu ao configurar a requisição
                     console.log('Erro ao configurar a requisição:', error.message);
                 }*/
-            }
-        }
-        loadLogins();
-    };
+      }
+    }
+    loadLogins();
+  }
 
-    return(
-        <header className="bg-[#5C832F] py-4 px-2 flex flex-col md:flex-row justify-between items-center" >
-            <Link to={'/'} className="mb-4 md:mb-0"><img src={logo}/></Link>
-             <div class="flex flex-col md:flex-row text-center md:space-x-4 items-center w-full md:w-auto">
-                <h5 className="text-white mb-2 md:mb-0">Acesso Restrito:</h5>
-                <input 
-                    type="email" 
-                    name="email" 
-                    class="h-8 px-2 mb-2 md:mb-0" 
-                    placeholder="usuário"
-                    value={login}
-                    onChange={(e)=> setLogin(e.target.value)}
-                />
-                <input 
-                    type="password" 
-                    name="senha" 
-                    class="h-8 px-2 mb-2 md:mb-0" 
-                    placeholder="senha" 
-                    value={login}
-                    onChange={e => setSenha(e.target.value)}
-                />
-                <button type="button"
-                    onClick={Entrar}
-                    class="bg-[#2c6825] text-white w-full md:w-auto rounded-xl h-8 duration-100 hover:bg-[#398830] font-bold px-4"
-                >
-                    Entrar
-                </button>
-            </div>
-        </header>
-    );
+  return (
+    <header className="bg-[#5C832F] py-4 px-2 flex flex-col md:flex-row justify-between items-center">
+      <Link to={"/"} className="mb-4 md:mb-0">
+        <img src={logo} />
+      </Link>
+      <div class="flex flex-col md:flex-row text-center md:space-x-4 items-center w-full md:w-auto">
+        <h5 className="text-white mb-2 md:mb-0">Acesso Restrito:</h5>
+        <input
+          type="email"
+          name="email"
+          class="h-8 px-2 mb-2 md:mb-0"
+          placeholder="usuário"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <input
+          type="password"
+          name="senha"
+          class="h-8 px-2 mb-2 md:mb-0"
+          placeholder="senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={Entrar}
+          class="bg-[#2c6825] text-white w-full md:w-auto rounded-xl h-8 duration-100 hover:bg-[#398830] font-bold px-4"
+        >
+          Entrar
+        </button>
+      </div>
+    </header>
+  );
 }
-
 
 export default Header;
--- src/pages/Descarte/index.js
+++ src/pages/Descarte/index.js
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import Carrosel from "../../components/Carrossel/Carrosel";
 import { Link } from "react-router-dom";
+import Carrosel from "../../components/Carrossel/Carrosel";
 
 function Descarte() {
   return (
@@ -10,23 +9,35 @@ function Descarte() {
       </div>
       <div className="flex flex-col px-4 sm:px-8 md:px-16 lg:px-36">
         {/* Divisão Quero descartar */}
-        <h1 className="text-[#5C832F] my-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">Quero descartar</h1>
+        <h1 className="text-[#5C832F] my-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          Quero descartar
+        </h1>
         {/* Cards */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
           <div className="bg-slate-400 h-36 sm:h-48 md:h-56 lg:h-72 w-full flex items-center justify-center hover:opacity-80">
-            <Link to="/mapa" state={{ linha: "Verde" }} className="text-white">Verde</Link>
+            <Link to="/mapa" state={{ linha: "Verde" }} className="text-white">
+              Verde
+            </Link>
           </div>
           <div className="bg-slate-400 h-36 sm:h-48 md:h-56 lg:h-72 w-full flex items-center justify-center hover:opacity-80">
-            <Link to="/mapa" state={{ linha: "Marron" }} className="text-white">Marron</Link>
+            <Link to="/mapa" state={{ linha: "Marrom" }} className="text-white">
+              Marrom
+            </Link>
           </div>
           <div className="bg-slate-400 h-36 sm:h-48 md:h-56 lg:h-72 w-full flex items-center justify-center hover:opacity-80">
-            <Link to="/mapa" state={{ linha: "Azul" }} className="text-white">Azul</Link>
+            <Link to="/mapa" state={{ linha: "Azul" }} className="text-white">
+              Azul
+            </Link>
           </div>
           <div className="bg-slate-400 h-36 sm:h-48 md:h-56 lg:h-72 w-full flex items-center justify-center hover:opacity-80">
-            <Link to="/mapa" state={{ linha: "Branco" }} className="text-white">Branco</Link>
+            <Link to="/mapa" state={{ linha: "Branco" }} className="text-white">
+              Branco
+            </Link>
           </div>
           <div className="bg-slate-400 h-36 sm:h-48 md:h-56 lg:h-72 w-full flex items-center justify-center hover:opacity-80">
-            <Link to="/mapa" state={{ linha: "Log" }} className="text-white">Logística Reversa</Link>
+            <Link to="/mapa" state={{ linha: "Log" }} className="text-white">
+              Logística Reversa
+            </Link>
           </div>
         </div>
       </div>
